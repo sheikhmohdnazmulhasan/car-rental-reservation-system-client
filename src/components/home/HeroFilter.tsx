17ebe54859a78370435457fe511a5b96712cb9f6
mdnazmul42726 <nazmulofficial@outlook.com>
2024-08-25 18:25:58 +0600

    get filter form data

--- src/components/home/HeroFilter.tsx
+++ src/components/home/HeroFilter.tsx
@@ -1,11 +1,11 @@
-// [#0C0C0C]
-
 import { DatePicker, Select } from "antd";
 import { TFilterFOptions } from "../../interface/filter.options.interface";
 import { heroDistrictFilterOptions } from "../../const/filter.district";
 import { heroColorFilterOptions } from "../../const/filter.colors";
+import { Controller, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const HeroFilter = () => {
+    const { control, handleSubmit } = useForm();
     const actualDistrictFilterOptions: TFilterFOptions[] = [];
     const actualColorFilterOptions: TFilterFOptions[] = [];
 
@@ -20,41 +20,67 @@ const HeroFilter = () => {
         actualColorFilterOptions.push({
             value: heroColorFilterOptions[i],
             label: heroColorFilterOptions[i],
-        })
+        });
+    }
+
+    //! actual action fn
+    const handleFormSubmit: SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
     }
 
     return (
-        <div className="w-96 hidden md:flex flex-col p-7  bg-rose-600 bg-opacity-20 bg -mt-40">
-
-            <form className="w-full space-y-2">
+        <div className="w-96 hidden md:flex flex-col p-7 bg-rose-600 bg-opacity-20 rounded-md -mt-40">
+            <form className="w-full space-y-2" onSubmit={handleSubmit(handleFormSubmit)}>
                 <div className="">
                     <p className="text-left mb-1">Location</p>
-                    <Select
-                        showSearch
-                        placeholder='Select Your Area'
-                        style={{ width: '100%' }}
-                        optionFilterProp="label"
-                        options={actualDistrictFilterOptions}
+                    <Controller
+                        name="location"
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                showSearch
+                                placeholder='Select Your Area'
+                                style={{ width: '100%' }}
+                                optionFilterProp="label"
+                                options={actualDistrictFilterOptions}
+                            />
+                        )}
                     />
                 </div>
 
                 <div className="">
                     <p className="text-left mb-1">Color</p>
-                    <Select
-                        showSearch
-                        placeholder='Select Your Favorite Color'
-                        style={{ width: '100%' }}
-                        optionFilterProp="label"
-                        options={actualColorFilterOptions}
+                    <Controller
+                        name="color"
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                showSearch
+                                placeholder='Select Your Favorite Color'
+                                style={{ width: '100%' }}
+                                optionFilterProp="label"
+                                options={actualColorFilterOptions}
+                            />
+                        )}
                     />
                 </div>
 
                 <div className="">
                     <p className="text-left mb-1">Date</p>
-                    <DatePicker
-                        style={{ width: '100%' }}
+                    <Controller
+                        name="date"
+                        control={control}
+                        render={({ field }) => (
+                            <DatePicker
+                                {...field}
+                                style={{ width: '100%' }}
+                            />
+                        )}
                     />
                 </div>
+
                 <div className="!mt-5">
                     <button type="submit" className="py-2 w-full text-center px-2 bg-rose-600 hover:bg-rose-700 transition-all rounded-md">Search</button>
                 </div>
